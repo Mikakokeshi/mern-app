cde3660426e7cd8aed99a8f46fd7b2a52d3b570d
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-09-21 12:50:44 +0800

    `initial`

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
-  port: process.env.PORT || 8000,
+  mongoURL: process.env.MONGO_URL || 'mongodb://Mikakokeshi:Z8LuGuge@cluster0-shard-00-00-x8veh.mongodb.net:27017,cluster0-shard-00-01-x8veh.mongodb.net:27017,cluster0-shard-00-02-x8veh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+  port: process.env.PORT || 7000,
 };
 
 export default config;
